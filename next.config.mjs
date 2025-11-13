fac276ac6c33a90a60800dba2ac74f72cb950b26
Wyeth <rellamaswyeth@gmail.com>
2025-11-13 22:11:33 +0800

    updated config and workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  export default nextConfig
-  
+  output: 'export',
+  basePath: '/yellow_jacket_toys_ph', // Added basePath for GitHub Pages
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
